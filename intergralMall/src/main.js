34d774f003eda421d87b74f836f0c43a9926816a
summer-rue <59637117+summer-rue@users.noreply.github.com>
2020-03-03 12:39:14 +0800

    基础配置

--- intergralMall/src/main.js
+++ intergralMall/src/main.js
@@ -4,6 +4,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import MetaInfo from 'vue-meta-info'
+import store from './store'
+import 'vant/lib/index.css'
 
 Vue.use(MetaInfo)
 
@@ -12,6 +14,7 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   router,
   components: { App },
   template: '<App/>'
